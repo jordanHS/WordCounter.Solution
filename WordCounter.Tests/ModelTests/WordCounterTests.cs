cc828014753687fe254990642de4b6eb8f284c25
Jordan Safford <jordansafford@gmail.com>
2020-01-19 20:54:43 -0800

    rewrite first test

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -4,13 +4,14 @@ using WordCounter.Models;
 namespace WordCounter.Tests
 {
     [TestClass]
-    public class SentenceTests
+    public class WordCountTests
     {
+
         [TestMethod]
-        public void Sentence_GetSentence_CreateSentence()
+        public void WordCount_GetSentence_CreateSentence()
         {
-           Sentence newSentence = new Sentence("The cat walked into the cathedral");
-           string result = newSentence.GetSentence();
+           WordCount newWordCount = new WordCount("The cat walked into the cathedral", "cat");
+           string result = newWordCount.GetSentence();
            Assert.AreEqual(result, "The cat walked into the cathedral");
         }
     }
